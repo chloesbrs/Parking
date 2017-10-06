fba717a3c5e6c8a8bbb4814c3bb102caeb5e4936
chloesbrs <chloe.sbrs@orange.fr>
2017-10-06 14:26:55 +0200

    obstart

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+session_start();
+    try
+            {
+                $bdd = new PDO("mysql:host=localhost;dbname=parking;charset=utf8","root","");
+            }
+            catch(Exception $e)
+            {
+				die("Erreur bdd non trouvée");
+            }
+
 if(!isset($_GET['p']) || $_GET['p'] == "")
 {
         $page = "accueil";
 }
-else 
+else
 {
     if(!file_exists("content/".$_GET['p'].".php"))
         {
@@ -12,8 +22,8 @@ else
         else
             $page = $_GET['p'];
     }
-ob_start();//suspend l'affichage
-    include "content/".$page.".php";
+    ob_start();//suspend l'affichage
+    require "content/".$page.".php";
     $content = ob_get_contents();//recuperer ce qui n'a pas ete affiché
     ob_end_clean();//reprend l'affichage
 
