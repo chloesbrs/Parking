30b5fa553c9c415fe7339a0278d1a4f03be88dc3
Chloesbrs <chloe.sbrs@orange.fr>
2017-10-13 12:38:18 +0200

    TEST1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
     try
             {
-                $bdd = new PDO("mysql:host=localhost;dbname=parking;charset=utf8","root","");
+                $bdd = new PDO("mysql:host=localhost;dbname=parking;charset=utf8","root","root");
             }
             catch(Exception $e)
             {
