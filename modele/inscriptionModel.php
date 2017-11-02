78d22843af7895d0f798e8db7acf83bf39a9bdeb
MarkoStefanovic-98 <markostef@hotmail.fr>
2017-11-02 15:27:54 +0100

    Maj

--- modele/inscriptionModel.php
+++ modele/inscriptionModel.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-function inscription($nom, $prenom, $adresse, $cp, $ville, $mail, $mdp)
-    
-    {
-            global $bdd;
-
-		    $bdd->exec("INSERT INTO user(nom,prenom,adresse,cp,ville,mail,mdp,attente,lvl) VALUES('".$nom."','".$prenom."','".$adresse."','".$cp."','".$ville."','".$mail."','".sha1($mdp)."',1,1)");
-
-			$_SESSION["connecte"]= true;
-			$_SESSION["lvl"]= 1;
-			$_SESSION["id_u"]= $bdd->lastInsertId();
-    }
-
-=======
 <?php
 
 function inscription($nom, $prenom, $mail, $mdp)
@@ -34,6 +18,3 @@ function inscription($nom, $prenom, $mail, $mdp)
         
         header('Location: index.php?p=accueil');
     }
-
->>>>>>> a4396901f93a30cbd78152ca77cf3325c1db828e
-?>
